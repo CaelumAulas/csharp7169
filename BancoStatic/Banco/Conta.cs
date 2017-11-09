818a56f5ad2ae43cf445c6646a4d44db05d37d99
Lucas Felix <lfelixsampaio@gmail.com>
2017-11-09 17:34:17 +0000

    Static

---
+++ BancoStatic/Banco/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Banco
+{
+    public abstract class Conta
+    {
+        public static int Total { get; private set; }
+
+        public Conta()
+        {
+            Total++;
+        }
+
+        public Cliente Titular { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; private set; }
+
+        public virtual void Deposita(double valor)
+        {
+            // Saldo = Saldo + valor;
+            Saldo += valor;
+        }
+
+        public virtual void Saca(double valor)
+        {
+            if(Saldo >= valor)
+            {
+                Saldo -= valor;
+
+            }
+        }
+    }
+}
